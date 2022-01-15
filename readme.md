9fbed8277be55b210179e62d3a603b638aa2b59c
Shigma <shigma10826@gmail.com>
2022-01-15 12:26:56 +0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# koishi-plugin-crazy-thursday
+ 
+[![npm](https://img.shields.io/npm/v/koishi-plugin-crazy-thursday?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-crazy-thursday)
+
+随机输出 KFC 疯狂星期四段子。
+
+语料来源：https://github.com/Nicr0n/fucking_crazy_thursday
+
+## 使用方法
+
+输入“疯狂星期四”即可。
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Context, Random, Schema } from 'koishi'
+
+const corpus: string[] = require('../corpus')
+
+export const name = 'crazy-thursday'
+
+export const schema = Schema.object({})
+
+export function apply(ctx: Context) {
+  ctx.command('thursday', '随机输出 KFC 疯狂星期四段子')
+    .shortcut('疯狂星期四')
+    .action(() => Random.pick(corpus))
+}
