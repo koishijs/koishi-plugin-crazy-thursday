858b88d7248abca2df8e25dc6a185cceedddbe0c
Shigma <1700011071@pku.edu.cn>
2022-02-12 22:13:32 +0800

    enhance schema

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Context, Random, Schema } from 'koishi'
 
-const corpus: string[] = require('../corpus')
+const corpus: string[] = require('./corpus')
+
+export interface Config {}
 
 export const name = 'crazy-thursday'
-
-export const schema = Schema.object({})
+export const schema: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   ctx.command('thursday', '随机输出 KFC 疯狂星期四段子')
